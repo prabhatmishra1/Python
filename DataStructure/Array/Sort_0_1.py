dac15ec6551d322073a1c3b025883e580203c260
prabhatmishra1 <mishraa.prabhat@gmail.com>
2022-11-18 17:40:41 +0530

    added optimized bubble sort

--- DataStructure/Array/Sort_0_1.py
+++ DataStructure/Array/Sort_0_1.py
@@ -1,3 +1,14 @@
+"""
+Sort 0 and 1 can be solved using binary search approach.
+How it works ?
+STEP:1- Take two pointer i and j where i will start from 0 and j will point to 
+last index.
+STEP:2- This is two pointer approach where i and j will travel i->[]<-j.
+STEP:3- If arr[i] has 0 increment i 
+STEP:4- if arr[j] has 1 increment j
+STEP:5 If arr[i]== 1 and arr[j] == 0 it means we need to swap them and increment both.
+"""
+
 def sort_0_1(arr):
 
     i, j = 0, len(arr)-1
